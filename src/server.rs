e832dc1558c3b52aace1b661a2978dd3606ce7a4
Bill Habermaas <bill@habermaas.us>
2025-03-03 21:56:56 -0500

    Add copyright headers

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2025 Habermaas Systems, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 use tonic::{transport::Server, Request, Response, Status};
 extern crate mongodb;
 use mongodb::Database;
@@ -12,7 +26,6 @@ mod users;
 
 //Todo Add setting of MOTD
 //Todo Finish getMOTD
-//Todo code for user update
 //Todo Add time stamps to user and motd records
 //Todo Add apache license headers to each source module
 
